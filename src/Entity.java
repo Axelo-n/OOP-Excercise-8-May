cc4b96303ca14ae85f105262abed71880a4f31a1
Axelo-n <mkev46p@gmail.com>
2025-05-08 09:06:08 +0700

    implemented inheritance and interfaces

--- src/Entity.java
+++ src/Entity.java
@@ -1,5 +1,5 @@
 
-public class Entity {
+public class Entity implements IDamageable, IAttack, IRecoverable, IHeal{
 	protected String name;
 	protected HP hp;
 	protected int att;
@@ -17,19 +17,31 @@ public class Entity {
 		this.def = def;
 	}
 	
-	public void takeDamage(int att) {
-		att -= this.def;
+	public int takeDamage(int att, int def) {
+		att -= def;
 		if(att < 0) att = 0;
 		hp.reduce(att);
 		System.out.println(this.name + " is taking damage for " + att + " point(s)");
+		return att;
 	}
 	
-	public void attack(Enemy enemy) {
+	public void attack(IDamageable damageable) {
+		Enemy enemy = (Enemy)damageable;
 		System.out.println(this.name + " attacking " + enemy);
-		enemy.takeDamage(this.att);
+		enemy.takeDamage(this.att, enemy.def);
 	}
 	
 	public void showHP() {
 		System.out.println(this.name + " current HP is " + this.hp);
+	}
+
+	@Override
+	public void heal(int value) {
+
+	}
+
+	@Override
+	public void recover(int value) {
+
 	}
 }
